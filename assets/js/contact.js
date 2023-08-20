5fb0616b51caffe4baa1cb9697e275c5a88da46f
Om soni <omsoni774600@gmail.com>
2023-08-20 13:51:21 +0530

    first commit

---
+++ assets/js/contact.js
@@ -0,0 +1,77 @@
+'use strict';
+//contact.js
+
+/**
+ * Initialize the contact form
+ * @param {document} document
+ * @param {formElementId} formElementId
+ */
+function initContactForm(document, formElementId) {
+    var form = document.getElementById(formElementId);
+    form.addEventListener("submit", submitContactForm);
+}
+
+
+function closeContactFormStatusAlert() {
+    let successStatus = document.getElementById("contact-form-status-alert");
+    let errorStatus = document.getElementById("contact-form-status-alert-error");
+    gsap.to(successStatus, { opacity: '0', ease: "easeOut", duration: 0.2 })
+    gsap.to(errorStatus, { opacity: '0', ease: "easeOut", duration: 0.2 })
+}
+
+
+
+async function submitContactForm(event) {
+    event.preventDefault();
+    var form = document.getElementById('contact-form');
+    var successStatus = document.getElementById("contact-form-status-alert");
+    var errorStatus = document.getElementById("contact-form-status-alert-error");
+    showLoadingMask();
+
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            // Success
+            gsap.to(successStatus, { opacity: '1', ease: "easeIn", duration: 0.2 })
+            form.reset();
+            hideLoadingMask();
+        } else {
+            // Error
+            response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                    gsap.to(errorStatus, { opacity: '1', ease: "easeIn", duration: 0.2 })
+                    console.log('Unknown error occurred on contact form...');
+                    form.reset();
+                    hideLoadingMask();
+                } else {
+                    gsap.to(errorStatus, { opacity: '1', ease: "easeIn", duration: 0.2 })
+                    console.log('Unknown error occurred on contact form...');
+                    form.reset();
+                    hideLoadingMask();
+                }
+            })
+        }
+    }).catch(error => {
+        gsap.to(errorStatus, { opacity: '1', ease: "easeIn", duration: 0.2 })
+        form.reset();
+        hideLoadingMask();
+    });
+}
+
+function showLoadingMask() {
+    const parentContainer = document.querySelector("#parent-container");
+    const template = document.querySelector("#loading-mask");
+    const node = template.content.firstElementChild.cloneNode(true);
+    parentContainer.appendChild(node);
+}
+
+function hideLoadingMask() {
+    const loadingOverlay = document.querySelector("#loading-mask");
+    loadingOverlay.remove();
+}
---
+++ assets/js/download.js
@@ -0,0 +1,31 @@
+'use strict';
+//download.js
+let downloadPageRecaptchaValidated = false;
+/* --------------- Handle download page recapatcha--------------- */
+/**
+ * Enable the resume download button
+ * @param {*} token 
+ */
+function enableResumeDownloadButton(token) {
+    downloadPageRecaptchaValidated = true;
+    $('#resume--download-icon').removeClass('fas fa-window-close');
+    $('#resume--download-icon').addClass('fas fa-download');
+
+    $('#resume--download-link').prop('disabled', false);
+    $('#resume--download-link').prop('href', "assets/img/final.pdf");
+    $('#resume--download-link').prop('download', "HarryDulaneyResume");
+
+}
+
+/**
+ * Disable the resume download button
+ */
+function disableResumeDownloadButton() {
+    downloadPageRecaptchaValidated = false;
+    $('#resume--download-icon').removeClass('fas fa-download');
+    $('#resume--download-icon').addClass('fas fa-window-close');
+
+    $('#resume--download-link').prop('disabled', true);
+    $('#resume--download-link').prop('href', "#");
+    $('#resume--download-link').prop('download', undefined);
+}

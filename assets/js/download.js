5a3805842ae93d662fa32702d37a60e48dee7a00
Om soni <omsoni774600@gmail.com>
2023-08-22 10:10:41 +0530

    add link to cryto project

--- assets/js/download.js
+++ assets/js/download.js
@@ -1,17 +1,12 @@
 'use strict';
 //download.js
-let downloadPageRecaptchaValidated = false;
-/* --------------- Handle download page recapatcha--------------- */
-/**
- * Enable the resume download button
- * @param {*} token 
- */
-function enableResumeDownloadButton(token) {
+
+ 
+function enableResumeDownloadButton() {
     downloadPageRecaptchaValidated = true;
     $('#resume--download-icon').removeClass('fas fa-window-close');
     $('#resume--download-icon').addClass('fas fa-download');
 
-    $('#resume--download-link').prop('disabled', false);
     $('#resume--download-link').prop('href', "assets/img/final.pdf");
     $('#resume--download-link').prop('download', "HarryDulaneyResume");
 
@@ -20,12 +15,12 @@ function enableResumeDownloadButton(token) {
 /**
  * Disable the resume download button
  */
-function disableResumeDownloadButton() {
-    downloadPageRecaptchaValidated = false;
-    $('#resume--download-icon').removeClass('fas fa-download');
-    $('#resume--download-icon').addClass('fas fa-window-close');
+// function disableResumeDownloadButton() {
+//     downloadPageRecaptchaValidated = false;
+//     $('#resume--download-icon').removeClass('fas fa-download');
+//     $('#resume--download-icon').addClass('fas fa-window-close');
 
-    $('#resume--download-link').prop('disabled', true);
-    $('#resume--download-link').prop('href', "#");
-    $('#resume--download-link').prop('download', undefined);
-}
+//     $('#resume--download-link').prop('disabled', true);
+//     $('#resume--download-link').prop('href', "#");
+//     $('#resume--download-link').prop('download', undefined);
+// }
